2bf6f6b27051ca291cc58d86aa390676de7b52b8
Michael Dormann <michaeldormann@Michaels-MBP.hsd1.fl.comcast.net>
2019-03-04 21:33:25 -0500

    Prod settings

---
+++ DjangoReact/settings/prod.py
@@ -0,0 +1,22 @@
+from .base import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['michaeldormann.dev', 'www.michaeldormann.dev', 'localhost']
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoreact',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
